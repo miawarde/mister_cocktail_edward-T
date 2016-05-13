44c7f8e716897b7cdc157f336c346c0f50748924
Mia Warde <mia.maria.warde@gmail.com>
2016-05-13 12:58:43 +0200

    Heroku ready

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 require "sprockets/railtie"
-require "attachinary/orm/active_record"
+require "attachinary/form/active_record"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
