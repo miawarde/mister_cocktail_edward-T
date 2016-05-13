b6e7536d570a665ca1855bc3a307ea0ec69d7ce8
Mia Warde <mia.maria.warde@gmail.com>
2016-05-13 11:27:13 +0200

    Mister Cocktail Edward

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,26 @@
+class DosesController < ApplicationController
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @ingredients = Ingredient.all
+      render 'cocktails/show'
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
